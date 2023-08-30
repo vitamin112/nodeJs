90789fda5434e3671f8b1fd974f681add481f140
Nguyễn Văn Lộc <88890670+vitamin112@users.noreply.github.com>
2023-08-30 13:40:09 +0700

    install prettier, husky

--- src/app/controllers/NewController.js
+++ src/app/controllers/NewController.js
@@ -1,12 +1,12 @@
 class NewsController {
-  // [GET] /news
-  index(req, res) {
-    res.render("news");
-  }
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
 
-  // [GET] /news/:slug
-  show(req, res) {
-    res.send("<h1>New deatail</h1>");
-  }
+    // [GET] /news/:slug
+    show(req, res) {
+        res.send('<h1>New deatail</h1>');
+    }
 }
 module.exports = new NewsController();
